a77303be746a3b501d0c53c64cae6add48ba97f9
JuanPabloRP <jpabloroldanp@gmail.com>
2023-07-07 11:42:37 -0500

    feat: structure and styles of about section

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -4,23 +4,41 @@ import PropTypes from 'prop-types';
 
 const About = ({ NAVBAR_HEIGHT }) => {
 	const About = styled.section`
+		box-sizing: border-box;
 		scroll-margin-top: ${NAVBAR_HEIGHT}px;
-		border-width: 1px;
 		min-height: calc(100vh - ${NAVBAR_HEIGHT}px);
+		height: fit-content;
+
+		& article {
+			box-shadow: inset 0px 0px 7px 0px #fb2576, 0px 0px 7px 0px #fb2576;
+		}
+
+		& h2 {
+			color: white;
+			text-shadow: 0 0 3px #000, 0 0 7px #fb2576, 0 0 10px #fb2576;
+		}
 	`;
 
 	return (
-		<About id="about" className="border-blue border-solid ">
-			<h2>About</h2>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem maiores
-				qui illo, aspernatur, corporis perspiciatis eius harum, accusamus
-				voluptate doloribus quidem! Aut, obcaecati earum nesciunt exercitationem
-				alias ut beatae reiciendis. Nihil, doloribus. Ea ipsa, veniam atque,
-				consequatur dolore autem nihil, quaerat corrupti ipsam maxime eaque. Non
-				impedit eveniet, odit corporis voluptas possimus. Qui nihil laborum
-				possimus illum facilis, deleniti dignissimos?
-			</p>
+		<About id="about" className="flex justify-center items-start py-3 md:items-center ">
+			<article className="border-white border-solid border-[1px] rounded-md mx-2  p-3 max-w-4xl">
+				<h2 className="text-4xl text-center font-black uppercase text-black p-5">
+					About me
+				</h2>
+				<p className="">
+					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
+					maiores qui illo, aspernatur, corporis perspiciatis eius harum,
+					accusamus voluptate doloribus quidem! Aut, obcaecati earum nesciunt
+					exercitationem alias ut beatae reiciendis. Nihil, doloribus. Ea ipsa,
+					veniam atque, consequatur dolore autem nihil, quaerat corrupti ipsam
+					maxime eaque. Non impedit eveniet, odit corporis voluptas possimus.
+					Qui nihil laborum possimus illum facilis, deleniti dignissimos? Lorem,
+					ipsum dolor sit amet consectetur adipisicing elit. Animi asperiores
+					sapiente, eos voluptatem dolores reprehenderit. Odio dolores, enim
+					tenetur suscipit nesciunt harum, incidunt, nostrum et cumque at ex
+					aliquid sapiente?
+				</p>
+			</article>
 		</About>
 	);
 };
