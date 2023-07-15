2d7292af30f8c6b397a89c134f26236749ab36d5
JuanPabloRP <jpabloroldanp@gmail.com>
2023-07-15 13:26:00 -0500

    feat: new styles to for the About section

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -1,6 +1,7 @@
 //import React from 'react';
 import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
+import hello from '../../assets/svg/hello.svg';
 
 const About = ({ NAVBAR_HEIGHT }) => {
 	const About = styled.section`
@@ -24,23 +25,28 @@ const About = ({ NAVBAR_HEIGHT }) => {
 			id="about"
 			className="flex justify-center items-start py-3 md:items-center "
 		>
-			<article className="border-white border-solid border-[1px] rounded-md mx-2  p-3 max-w-4xl md:p-20">
+			<article className="border-white border-solid border-[1px] rounded-md mx-2  p-3 max-w-4xl md:p-14 lg:p-20">
 				<h2 className="text-4xl text-center font-black uppercase text-black p-5 tracking-wide">
 					About me
 				</h2>
-				<p>
-					Lorem ipsum dolor sit, amet consectetur adipisicing elit. Autem
-					maiores qui illo, aspernatur, corporis perspiciatis eius harum,
-					accusamus voluptate doloribus quidem! Aut, obcaecati earum nesciunt
-					exercitationem alias ut beatae reiciendis. Nihil, doloribus. Ea ipsa,
-					veniam atque, consequatur dolore autem nihil, quaerat corrupti ipsam
-					maxime eaque. Non impedit eveniet, odit corporis voluptas possimus.
-					Qui nihil laborum possimus illum facilis, deleniti dignissimos? Lorem,
-					ipsum dolor sit amet consectetur adipisicing elit. Animi asperiores
-					sapiente, eos voluptatem dolores reprehenderit. Odio dolores, enim
-					tenetur suscipit nesciunt harum, incidunt, nostrum et cumque at ex
-					aliquid sapiente?
-				</p>
+				<div className="flex flex-col justify-center items-center lg:flex-row ">
+					<img
+						src={hello}
+						alt="Hello image"
+						className="max-w-xs w-56 p-2 md:mr-4"
+					/>
+					<p className="p-2">
+						Juan Pablo Roldan Patiño is a software developer who lives in
+						Medellín, interested in gaining work experience, he has knowledge in
+						programming languages such as JavaScript, C# and Java, also in
+						languages such as HTML, CSS and SQL. He studies “Tecnología en
+						Desarrollo de Software (I.U. Pascual Bravo)”, and has participated
+						in renowned bootcamps in Colombia such as “Misión TIC (Mintic y
+						UdeA)” and “NODO (EAFIT)”. Among his skills that stand out the most
+						we find teamwork, continuous learning and the achievement of
+						objectives
+					</p>
+				</div>
 			</article>
 		</About>
 	);
