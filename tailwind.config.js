c8f35b239b021a629e84cf88994eb8fafeb72542
Juan Pablo RP <jpabloroldanp@gmail.com>
2024-06-11 18:43:11 -0500

    Agregando el meteor effect

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,21 @@ export default {
 				blue: '#00ADB5',
 				pink: '#FB2576',
 			},
+			animation: {
+				meteor: 'meteor 5s linear infinite',
+			},
+			keyframes: {
+				meteor: {
+					'0%': { transform: 'rotate(215deg) translateX(0)', opacity: 1 },
+					'70%': { opacity: 1 },
+					'100%': {
+						transform: 'rotate(215deg) translateX(-500px)',
+						opacity: 0,
+					},
+				},
+			},
 		},
 		fontFamily: {},
-		
 	},
 	plugins: [tailwindcssAnimated],
 	extend: {
